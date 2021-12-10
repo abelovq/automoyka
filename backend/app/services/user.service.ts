c21916857402dd7cea5c2d5a05278b06997e4c8f
Vladislav Zubov <v.zubov@leviossa.net>
2021-12-10 18:04:10 +0300

    back init

---
+++ backend/app/services/user.service.ts
@@ -0,0 +1,70 @@
+import { User } from "@prisma/client";
+import HttpException from "../exceptions/HttpExceptions";
+import prisma from "../prisma";
+
+class UserService {
+
+  async createUser(userData: User): Promise<User> {
+ 
+
+    return await prisma.user
+      .create({
+        data: {
+          ...userData,
+        },
+      })
+      .catch((ere) => {
+        console.log(err);
+        throw new HttpException(400, "Cannot create user");
+      });
+  }
+
+  async getUserById(id: number): Promise<User | null> {
+    return await prisma.user
+      .findFirst({
+        where: {
+          id,
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new HttpException(404, "User is not found");
+      });
+  }
+
+  async updateUser(userData: Partial<User>): Promise<User> {
+    const { id, ...updatingData } = userData;
+    const { name } = updatingData;
+
+    if (name !== undefined && !name) {
+      throw new HttpException(400, "name field cannot be empty");
+    }
+
+    return await prisma.user
+      .update({
+        where: {
+          id,
+        },
+        data: updatingData,
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new HttpException(400, "Cannot update user");
+      });
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    return await prisma.user
+      .delete({
+        where: {
+          id,
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new HttpException(404, "User is not find");
+      });
+  }
+}
+
+export default new UserService();
