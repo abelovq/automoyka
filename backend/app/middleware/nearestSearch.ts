6a8385104611e1374da2bf06c7bf74f0955b0082
Dmitry Gusev <dmitry@leviossa.net>
2021-12-11 23:57:23 +0300

    refactor

--- backend/app/middleware/nearestSearch.ts
+++ backend/app/middleware/nearestSearch.ts
@@ -19,18 +19,18 @@ export const getNearestCarWash = (carWashes: Car_Wash[], user: number[]) => {
   return nearestWash;
 };
 function getDistanceFromLatLonInKm(
-  lat1: number,
-  lon1: number,
-  lat2: number,
-  lon2: number
+  userLat: number,
+  userLon: number,
+  washLat: number,
+  washLon: number
 ) {
   const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1); // deg2rad below
-  const dLon = deg2rad(lon2 - lon1);
+  const dLat = deg2rad(washLat - userLat); // deg2rad below
+  const dLon = deg2rad(washLon - userLon);
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
+    Math.cos(deg2rad(userLat)) *
+      Math.cos(deg2rad(washLat)) *
       Math.sin(dLon / 2) *
       Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
